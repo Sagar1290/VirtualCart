146b28ffe889a20f7c8d7d0ecca3aefbce526ef7
yashrepogit <yas@cloudify.biz>
2025-08-31 15:15:20 +0530

    initialised: new frontent folder using vite+react

---
+++ cartapp/src/components/BarcodeInput.jsx
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+
+const BarcodeInput = ({ onBarcode }) => {
+    const inputRef = useRef();
+
+    const handleInput = (e) => {
+        const value = e.target.value.trim();
+        if (value.length >= 8) {
+            onBarcode(value);
+            inputRef.current.value = "";
+        }
+    };
+
+    return (
+        <input
+            ref={inputRef}
+            className="border p-2 rounded w-64"
+            placeholder="Scan or enter barcode"
+            autoFocus
+            onChange={handleInput}
+        />
+    );
+}
+
+export default BarcodeInput;
